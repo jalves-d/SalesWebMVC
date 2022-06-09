5236601eab7868fa98689aac17260af7bd2a010c
jalbuquerque <joao.neto.externo@ageas.pt>
2022-06-09 09:11:27 +0100

    new theme and new entities

---
+++ SalesWebMvc/Data/SeedingService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+        private SalesWebMvcContext _context;
+        public SeedingService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+        public void Seed()
+        {
+            if(_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any()) 
+            {
+                return;
+            }
+        }
+    }
+}
