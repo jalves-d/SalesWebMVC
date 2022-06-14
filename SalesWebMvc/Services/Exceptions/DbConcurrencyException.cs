6ea0c41ef9ede4123229a148175085b630c94c29
jalbuquerque <joao.neto.externo@ageas.pt>
2022-06-14 11:37:48 +0100

    Searchs added, Project Finished!

--- SalesWebMvc/Services/Exceptions/DbConcurrencyException.cs
+++ SalesWebMvc/Services/Exceptions/DbConcurrencyException.cs
@@ -3,7 +3,7 @@ using System.Runtime.Serialization;
 
 namespace SalesWebMvc.Services
 {
-    public class DbConcurrencyException : Exception
+    public class DbConcurrencyException : ApplicationException
     {
         public DbConcurrencyException(string message) : base(message)
         {
---
+++ SalesWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SalesWebMvc.Services
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
